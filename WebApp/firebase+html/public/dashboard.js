2c1b7b1e71fb474bcea5fa71e31a0a5ebf3b6cdf
alexmalyn <59982021+alexmalyn@users.noreply.github.com>
2020-04-26 20:10:37 -0400

    room names work

--- WebApp/firebase+html/public/dashboard.js
+++ WebApp/firebase+html/public/dashboard.js
@@ -1,15 +1,16 @@
 var ssid = 0;
-var macAdd = 0;
+var macAdds = [];
 var updates = {};
 var userInfo;
+var roomname = [];
 
 //this function should reconnect new SSID and potentially populate new roomCount
 function passSSID() {
 	if (ssid == 0) {
-		console.log("ssid is default");
+		//console.log("ssid is default");
 		var x = document.getElementById("SSID").value;
 	} else {
-		console.log("ssid is " + ssid);
+		//console.log("ssid is " + ssid);
 		var x = ssid;
 	}
 	userInfo = firebase.auth().currentUser;
@@ -24,12 +25,10 @@ function passSSID() {
 			if (snapshot.val() == null)
 				alert("Invalid SSID");
 			else {
-				alert("Rooms Created");
 				updates[ssidPath + '/roomCount'] = snapshot.val();
-				htmlnodecreation(snapshot.val());
-				//^^ this is the function that needs to be worked on
-				//probably need to set a small timeout for below function after nodes are populated
+				htmlnodecreation(uid, snapshot.val());
 				setTimeout(() => {
+					setroomnames(snapshot.val());
 					updateAccount(x, uid);
 				}, 100);
 				
@@ -43,21 +42,32 @@ function passSSID() {
 	}, 200);
 }
 
+function setroomnames(roomnum) {
+	
+	for (var i = 0; i < roomnum; i++)
+	{
+		var roomstrtitleid = "room_" + i + "_title";
+		console.log("roomname[" + i + "] is " + roomname[i]);
+		document.getElementById(roomstrtitleid).textContent = roomname[i];
+	}
+}
+
 //updates roomMacAddresses
 function updateAccount(ssid, uid) {
-	console.log("starting updateAccount");
+	//console.log("starting updateAccount");
 	var count = 0;
 	var arr = [];
 	var macAddRef = firebase.database().ref("ssids/" + ssid);
 	macAddRef.once('value',function(snapshot) {
 		arr = Object.keys(snapshot.val());	
 		arr.pop();
+		macAdds = arr;
 	});
 	setTimeout(() => {
 		var x,y;
 		while (count < arr.length) {
-			console.log("inside for loop with count = " + count);
-			console.log("arr[count] is " + arr[count]);
+			//console.log("inside for loop with count = " + count);
+			//console.log("arr[count] is " + arr[count]);
 			//listeners
 			var humstr = "hum" + count;
 			var tempstr = "temp" + count;
@@ -70,7 +80,7 @@ function updateAccount(ssid, uid) {
 			updates["users/" + uid + "/rooms/" + count + "/macAddress"] = arr[count];
 			count++;
 		}
-		console.log("leaving updateAccount");
+		//console.log("leaving updateAccount");
 		return firebase.database().ref().update(updates);
 	},50);	
 }
@@ -78,7 +88,7 @@ function updateAccount(ssid, uid) {
 function humlisten(humstr, humRef, uid, count) {
 	var x;
 	humRef.on('value', function(snapshot) {
-		console.log("humsnapshot.val is " + snapshot.val());
+		//console.log("humsnapshot.val is " + snapshot.val());
 		updates["users/" + uid + "/rooms/" + count + "/latestHumid"] = snapshot.val();
 		x = document.getElementById(humstr);
 		x.innerText = snapshot.val();
@@ -89,7 +99,7 @@ function humlisten(humstr, humRef, uid, count) {
 function templisten(tempstr, tempRef, uid, count) {
 	var y;
 	tempRef.on('value', function(snapshot) {
-		console.log("tempsnapshot.val is " + snapshot.val());
+		//console.log("tempsnapshot.val is " + snapshot.val());
 		updates["users/" + uid + "/rooms/" + count + "/latestTemp"] = snapshot.val();
 		y = document.getElementById(tempstr);
 		y.innerText = snapshot.val();
@@ -97,12 +107,13 @@ function templisten(tempstr, tempRef, uid, count) {
 	});
 }
 
-function htmlnodecreation(roomnum) {
+function htmlnodecreation(uid, roomnum) {
 	for (var i = 0; i < roomnum; i++) {	
-		console.log("in html with i=" + i);
 		var humstr = "hum" + i;
 		var tempstr = "temp" + i;
 		var roomstr = "room_" + i;
+		var roomstrtitle = roomstr + "_title";
+		var detstring = "det_" + i;
 		var linebr = document.createElement("br");
 		
 		var div = document.getElementById(roomstr);//HERE
@@ -113,7 +124,14 @@ function htmlnodecreation(roomnum) {
 				subdiv2.setAttribute('class','card-body');
 					var h4 = document.createElement("h4");
 					h4.setAttribute('class','card-title');
-					h4.textContent = "Default Room";
+					h4.setAttribute('id',roomstrtitle);
+					(function(index) {
+						let roomnameRef = firebase.database().ref("users/" + uid + "/rooms/" + index + "/roomName");
+						roomnameRef.once('value',function(snapshot) {
+							console.log("i = " + index + " and roomname is " + snapshot.val());
+							roomname[index] = snapshot.val();
+						});	
+					})(i);					
 					subdiv2.appendChild(h4);
 					
 					var p = document.createElement("p");
@@ -140,23 +158,50 @@ function htmlnodecreation(roomnum) {
 					p.appendChild(deg);
 				var subdiv3 = document.createElement("div");
 				subdiv3.setAttribute('class','card-footer');
-					var details = document.createElement("a");
-					details.setAttribute('href','https://humidibot-8a6ff.firebaseapp.com/roomview.html');
+					var details = document.createElement("BUTTON");
+					details.setAttribute('id',detstring);
+					//details.setAttribute("onclick","sessionSave("+uid+","+i+")");
 					details.setAttribute('class','btn btn-primary');
-					details.textContent = "Details";
+					details.innerHTML = "Details";
 					subdiv3.appendChild(details);
 			subdiv1.appendChild(subdiv2);
 			subdiv1.appendChild(subdiv3);
 		div.appendChild(subdiv1);
+		(function(index) {
+			let detstring = "det_" + index;
+			document.getElementById(detstring).addEventListener("click",
+				function() {
+					let roomiden = detstring[4];
+					sessionSave(uid,roomiden);
+			});
+			
+			
+		})(i);
 	}
+}
+
+function sessionSave(userid, i) {
+	//console.log("in session save with userid = " + userid + " and with room_num = " + i);
+	sessionStorage.setItem('uid',userid);
+	sessionStorage.setItem('i',i);
+	console.log("macAdds are " + macAdds);
+	sessionStorage.setItem('macAdd',macAdds[i]);
+	sessionStorage.setItem('ssid',ssid);
+	
+	setTimeout(() => {
+		console.log("leaving...");
+		window.location.replace("https://humidibot-8a6ff.firebaseapp.com/roomview.html");
+	},100);
 }
 
 function readSSID(ssidRef) {
 	ssidRef.once('value',function(snapshot) {
-		console.log("inside readSSID snap is " + snapshot.val());
+		//console.log("inside readSSID snap is " + snapshot.val());
 		ssid = snapshot.val();
 	});
 }
+
+
 
 function readmacAdd(macAddRef) {
 	macAddRef.once('value',function(snapshot) {
@@ -164,17 +209,12 @@ function readmacAdd(macAddRef) {
 	});	
 }
 
-//authentication listener, kind of broken as it triggers even when
-//changing webpages for some odd reason.
-
-
 document.addEventListener("DOMContentLoaded", function() {
 	firebase.auth().onAuthStateChanged(function(user) {
 		userInfo = firebase.auth().currentUser;
 	});
 	
 	setTimeout(() => {
-		console.log("about to call setup()");
 		setup();
 	}, 200);
 });
@@ -182,13 +222,10 @@ document.addEventListener("DOMContentLoaded", function() {
 //first load setup
 function setup() {
 	if (userInfo) {
-	    console.log("in auth (user)");
-		var uid = userInfo.uid;
+	    var uid = userInfo.uid;
 		var ssidRef = firebase.database().ref("users/" + uid + "/SSID");
 		readSSID(ssidRef);
 		setTimeout(() => {
-			console.log("about to passSSID");
-			console.log("ssid is " + ssid);
 			passSSID();
 		},200);
 	} else {
--- WebApp/firebase+html/public/room.js
+++ WebApp/firebase+html/public/room.js
@@ -1,15 +1,112 @@
-var ssid = 0;
-var macAdd = 0;
+var uid = sessionStorage.getItem('uid');
+var roomnum = sessionStorage.getItem('i');
+
+var ssid = sessionStorage.getItem('ssid');
+var macAdd = sessionStorage.getItem('macAdd');
+console.log("ssid is " + ssid + " and macAdd is " + macAdd);
 var updates = {};
+var refpath = "parameters/" + uid + "/" + roomnum;
+var REF = firebase.database().ref(refpath);
+var params = [];
+var dehum, door, fan, humid, win;
+var roomname = document.getElementById("roomname");
+var humPath = "ssids/" + ssid + "/" + macAdd + "/latestHumid";
+var tempPath = "ssids/" + ssid + "/" + macAdd + "/latestTemp";
+var humRef = firebase.database().ref(humPath);
+var tempRef = firebase.database().ref(tempPath);
+
+document.addEventListener("DOMContentLoaded", function() {
+	humlisten();
+	templisten();
+	readRoomName();
+	REF.once('value',function(snapshot) {
+		params = Object.values(snapshot.val());
+		console.log(params);
+	});
+	setTimeout(() => {
+		console.log(params);
+		paramSetup();
+	}, 600);
+});
+
+roomname.addEventListener("input", function() {
+	updates["users/" + uid + "/rooms/" + roomnum + "/roomName"] = roomname.textContent;	
+	setTimeout(() => {
+		return firebase.database().ref().update(updates);
+	}, 200);
+});
+
+function paramSetup() {
+	dehum = document.getElementById("dehumidifier");
+	door = document.getElementById("door");
+	fan = document.getElementById("fan");
+	humid = document.getElementById("humidifier");
+	win = document.getElementById("window");	
+	
+	if (params[0] == true) {
+		dehum.setAttribute("checked","true");
+		dehum.checked = true;
+	}
+	if (params[1] == true) {
+		door.setAttribute("checked","true");
+		door.checked = true;
+	}
+	if (params[2] == true) {
+		fan.setAttribute("checked","true");
+		fan.checked = true;
+	}
+	if (params[3] == true) {
+		humid.setAttribute("checked","true");
+		humid.checked = true;
+	}
+	if (params[4] == true) {
+		win.setAttribute("checked","true");
+		win.checked = true;
+	}
+
+}
 
 function updateParams() {
-	if ( document.getElementById("door").checked == true) {
-		
-	} else {
-		
-		document.getElementById("navtop").style.backgroundColor = "#42c5f5";
-		document.getElementById("footercolor").style.backgroundColor = "#42c5f5";
-	}
+	console.log("in updateParams");
+	
+	dehum = document.getElementById("dehumidifier").checked;
+	door = document.getElementById("door").checked;
+	fan = document.getElementById("fan").checked;
+	humid = document.getElementById("humidifier").checked;
+	win = document.getElementById("window").checked;	
+
+	updates[refpath + "/dehumidifier"] = dehum;
+	updates[refpath + "/door"] = door;
+	updates[refpath + "/fan"] = fan;
+	updates[refpath + "/humidifier"] = humid;
+	updates[refpath + "/window"] = win;
+	
+	setTimeout(() => {
+		return firebase.database().ref().update(updates);
+	}, 200);
+}
+
+function humlisten() {
+	var x = document.getElementById("humidityRead");
+	humRef.on('value', function(snapshot) {
+		console.log("humidity is reading " + snapshot.val());
+		x.innerText = snapshot.val();
+	});
+}
+
+function templisten() {
+	var y = document.getElementById("tempRead");
+	tempRef.on('value', function(snapshot) {
+		y.innerText = snapshot.val();
+	});
+}
+
+function readRoomName() {
+	let z = document.getElementById("roomname");
+	let roomnameRef = firebase.database().ref("users/" + uid + "/rooms/" + roomnum + "/roomName");
+	roomnameRef.once('value',function(snapshot) {
+		z.innerText = snapshot.val();
+	});	
 }
 
 function toggleMode() {
